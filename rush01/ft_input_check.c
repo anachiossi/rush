b12a1e308b1ce8235953a8f3bb566aa69b22d925
Ana Carolina De Morais Chiossi <anade-mo@e3r1p8.42roma.it>
2026-03-21 12:28:46 +0100

    added test log folder

--- rush01/ft_input_check.c
+++ rush01/ft_input_check.c
@@ -6,10 +6,11 @@
 /*   By: anade-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 08:42:04 by anade-mo          #+#    #+#             */
-/*   Updated: 2026/03/21 09:10:25 by anade-mo         ###   ########.fr       */
+/*   Updated: 2026/03/21 09:52:28 by anade-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
+
+//#include <unistd.h>
 
 int	ft_strlen(char *str);
 
@@ -24,15 +25,16 @@ int	ft_input_check(int argc, char *str)
 		return (0);
 	while (str[i])
 	{
-		if ((str[i] < '1' || str[i] > '4') 
-				&& (str[i] != ' '))
+		if ((str[i] < '1' || str[i] > '4') && (str[i] != ' '))
 			return (0);
 		i++;
 	}
 	return (1);
 }
-
+/*
 int	main(int argc, char **argv)
 {
-	ft_input_check(argc, argv[1]);
-}
+	if (ft_input_check(argc, argv[1]) == 0)
+		write(1, "ERROR\n", 6);
+	return (0);
+}*/
