decafefd8e0eda54d2c87d66f4fa8d3900f56818
anachiossi <anachiossi@gmail.com>
2026-03-22 15:51:29 +0100

    my solve code is getting to complicated

--- rush01/solve.c
+++ rush01/solve.c
@@ -17,8 +17,58 @@ cel = 15
 from main.c cell = 0
 */
 
-int	check_repeat
+int	check_repeat(int grid[4][4], int col, int row, int nbr)
+{
+}
+	int	i;
+	
+	i = 0;
+	while (i < 4)
+	{
+		if (grid[row][i] == nbr)
+			return (1);
+		i++;
+	}
+	i = 0;
+	while (i < 4)
+	{
+		if (grid[i][col] == nbr)
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
+int	check_rows(int grid[4][4], int clues[16])
+{
+	int	i++;
+	int	j++;
+	int	sequence[4];
+
+	i = 0;
+	while (i < 4) //row 00, 01, 02, 03, left 8a11] //
+	{
+		if (count_visible(grid[i]) != clues[8 + i])
+			return (0);
+		i++;
+	}
+	i = 0;
+	while (i < 4) //row 03, 02, 01, 00. right 12a15 //
+	{
+		j = 0;
+		while (j < 4)
+		{
+			sequence[j] = grid[i][3 - j]
+			j++;
+		}
+		if (count_visible(sequence) != clues[12 + i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+//stoped here, realized check grid was too long so i split in chec rows, check cols
+		
 int	solve(int grid[4][4], int clues[16], int cell)
 {
 	int	row;
@@ -32,11 +82,12 @@ int	solve(int grid[4][4], int clues[16], int cell)
 		return (1);
 	while (nbr <= 4)
 	{
-		if (nbr != grid[row] && nbr != grid[col])
+		if (check_repeat(grid, col, row, nbr) == 0)
 		{
 			grid[row][col] = nbr;
 			if (solve(grid, clues, cell + 1) == 1)
-				return (1);
+			return (1);
+		}
 		nbr++;
 	}
 	grid[row][col] = 0;
